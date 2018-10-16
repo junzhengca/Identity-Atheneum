ac787e5f21ed023d6411c2e912f5736649929997
Jun Zheng <me@jackzh.com>
2018-10-16 09:01:25 -0400

    Add landing page and build scripts

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "$(date) Begin build..."
+
+echo "$(date) Transpiling JavaScript..."
+flow-remove-types ./src/ -d ./lib/ --all --pretty &>/dev/null
+echo "$(date) JavaScript Transpiled."
+
+echo "$(date) Copying views..."
+cp src/views lib/views &>/dev/null
+echo "$(date) Views copied."
+
+echo "$(date) Building static assets..."
+cd landing
+bundle exec jekyll build &>/dev/null
+cd ..
+echo "$(date) Static assets built."
