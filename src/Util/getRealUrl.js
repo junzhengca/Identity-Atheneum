34d47258324a13ec17def0dd1df5547a3661d1f9
Jun Zheng <me@jackzh.com>
2018-11-19 20:04:43 -0500

    ☕ Add test for Util/getRealUrl

--- src/Util/getRealUrl.js
+++ src/Util/getRealUrl.js
@@ -5,8 +5,9 @@ const config = require('../config');
 /**
  * Returns the real absolute URL
  * @param url
+ * @param _config
  * @returns {*}
  */
-module.exports = function(url: string): string {
-    return config.host_root + url;
+module.exports = function(url: string, _config: any = null): string {
+    return (_config || config).host_root + url;
 };
